af9f93a7cb869779f64c9997e4476e87e7d558de
Mohamed khedr <khedrkhedr370@gmail.com>
2024-08-06 19:03:59 +0300

    search about city and

--- app/src/main/java/com/example/klivvrproject/MainActivity.kt
+++ app/src/main/java/com/example/klivvrproject/MainActivity.kt
@@ -2,38 +2,73 @@ package com.example.klivvrproject
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.widget.addTextChangedListener
+import androidx.lifecycle.lifecycleScope
+
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.klivvrproject.adapter.CityAdapter
 import com.example.klivvrproject.databinding.ActivityMainBinding
 import com.example.klivvrproject.model.City
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import kotlinx.serialization.json.Json
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var cities: List<City>
+
     private lateinit var binding: ActivityMainBinding
     private lateinit var cityAdapter: CityAdapter
+    private lateinit var cities: List<City>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        binding =ActivityMainBinding.inflate(layoutInflater)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        cities = loadCitiesFromAssets(this).sortedBy { it.name.lowercase()}
-        binding.rvCities.layoutManager = LinearLayoutManager(this)
-        cityAdapter = CityAdapter(cities)
-        binding.rvCities.adapter = cityAdapter
+
+        cityAdapter = CityAdapter(emptyList())
+
+
+        lifecycleScope.launch {
+            try {
+                cities = loadCitiesFromAssets(this@MainActivity).sortedBy { it.name.lowercase() }
+                withContext(Dispatchers.Main) {
+                    cityAdapter = CityAdapter(cities)
+                    binding.rvCities.layoutManager = LinearLayoutManager(this@MainActivity)
+                    binding.rvCities.adapter = cityAdapter
+                    binding.progressBar.visibility = View.GONE
+                }
+            } catch (e: Exception) {
+                withContext(Dispatchers.Main) {
+                    binding.progressBar.visibility = View.GONE
+                }
+            }
+        }
+
+             binding.searchInput.addTextChangedListener {text ->
+                filterCities(text.toString())
+             }
     }
-    
+
     private fun loadCitiesFromAssets(context: Context): List<City> {
         val inputStream = context.assets.open("cities.json")
-        val jsonString = inputStream.bufferedReader().use {
-            it.readText() }
+        val jsonString = inputStream.bufferedReader().use { it.readText() }
         return Json.decodeFromString(jsonString)
+    }
+
+
+
+    private fun filterCities(prefix: String) {
+        val filteredCities = cities.filter {
+            it.name.startsWith(prefix, ignoreCase = true)
+        }
+        cityAdapter.updateData(filteredCities)
     }
 
 }
